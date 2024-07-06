e56a44600a5d42b4b7806a01f7d81901402a303d
Szymon Hennel <szymon.hennel@gmail.com>
2024-07-06 18:38:19 +0200

    Fully working version with argument validation and well supported -WhatIf

--- public/Set-FileDates.ps1
+++ public/Set-FileDates.ps1
@@ -5,49 +5,52 @@ function Set-FileDates {
         [string]$arg1,
 
         [Parameter(Mandatory=$true)]
-        [string]$arg2
+        [string]$arg2,
+
+        [Parameter(Mandatory=$false)]
+        [switch]$AlignToLastWrite
     )
 
-    # Check if arg1 and arg2 are files
-    $whatIfMessage = if ($AlignToLastWrite) {
-        "Set CreationTime and LastWriteTime equal to LastWriteTime of $arg2"
-    } else {
-        "Set CreationTime and LastWriteTime equal to CreationTime and LastWriteTime of $arg2"
-    }
-
-    if ($PSCmdlet.ShouldProcess("$arg1", $whatIfMessage)) {
-        if ((Test-Path $arg1 -PathType Leaf) -and (Test-Path $arg2 -PathType Leaf)) {
+    if (Test-Path $arg1 -PathType Leaf) {
+        if (Test-Path $arg2 -PathType Leaf) {
             $file1 = Get-Item $arg1
             $file2 = Get-Item $arg2
 
             if ($AlignToLastWrite) {
                 # Set the CreationTime and LastWriteTime of file1 to the LastWriteTime of file2
-                $file1.CreationTime = $file2.LastWriteTime
-                $file1.LastWriteTime = $file2.LastWriteTime
-                Write-Verbose "Aligned CreationTime and LastWriteTime of $file1 to LastWriteTime of $file2"
+                if ($PSCmdlet.ShouldProcess("$arg1", "Set CreationTime and LastWriteTime equal to LastWriteTime of $file2")) {
+                    $file1.CreationTime = $file2.LastWriteTime
+                    $file1.LastWriteTime = $file2.LastWriteTime
+                    Write-Verbose "Aligned CreationTime and LastWriteTime of $file1 to LastWriteTime of $file2"
+                }
             } else {
                 # Set the CreationTime and LastWriteTime of file1 to the CreationTime and LastWriteTime of file2
-                $file1.CreationTime = $file2.CreationTime
-                $file1.LastWriteTime = $file2.LastWriteTime
-                Write-Verbose "Set CreationTime and LastWriteTime of $file1 to CreationTime and LastWriteTime of $file2"
+                if ($PSCmdlet.ShouldProcess("$arg1", "Set CreationTime and LastWriteTime equal to CreationTime and LastWriteTime of $file2")) {
+                    $file1.CreationTime = $file2.CreationTime
+                    $file1.LastWriteTime = $file2.LastWriteTime
+                    Write-Verbose "Set CreationTime and LastWriteTime of $file1 to CreationTime and LastWriteTime of $file2"
+                }
             }
         }
         # Check if arg2 is a datetime or ISO string
         else {
-            # Added a variable to hold the parsed datetime
-            $date = $null
-            # Changed the TryParse method call to use the $date variable as [ref]
-            if ([datetime]::TryParse($arg2, [ref]$date)) {
+            # Try to parse arg2 as a datetime
+            try {
+                $date = [datetime]::Parse($arg2)
                 $file1 = Get-Item $arg1
 
                 # Set the CreationTime and LastWriteTime of file1 to the specified date
-                $file1.CreationTime = $date
-                $file1.LastWriteTime = $date
-                Write-Verbose "Set CreationTime and LastWriteTime of $file1 to $date"
+                if ($PSCmdlet.ShouldProcess("$arg1", "Set CreationTime and LastWriteTime equal to $date")) {
+                    $file1.CreationTime = $date
+                    $file1.LastWriteTime = $date
+                    Write-Verbose "Set CreationTime and LastWriteTime of $file1 to $date"
+                }
             }
-            else {
+            catch {
                 Write-Error "arg2 must be a valid file path or a datetime/ISO string."
             }
         }
+    } else {
+        Write-Error "arg1 must be a valid file path."
     }
 }
